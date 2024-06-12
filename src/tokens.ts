914296a44ffa10ccfaf900d32bae358e4f3fdff8
aamir <aamirkhan@engineer.com>
2024-06-12 12:04:39 +0500

    feat: updated tokens

--- src/tokens.ts
+++ src/tokens.ts
@@ -1,7 +1,7 @@
 export const tokenDetails = {
 	usdt: {
 		address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
-		decimals: 8,
+		decimals: 6,
 		name: "tether",
 	},
 	shib: {
@@ -10,15 +10,50 @@ export const tokenDetails = {
 		name: "shiba inu",
 	},
 	dai: {
-		address: "",
-		decimals: 8,
-		name: "tether",
+		address: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+		decimals: 18,
+		name: "Dai Stable coin",
 	},
-	usdc: {
-		address: "",
+	weth: {
+		address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+		decimals: 18,
+		name: "Wrapped Ether",
+	},
+	wbtc: {
+		address: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
 		decimals: 8,
-		name: "tether",
+		name: "Wrapped BTC",
+	},
+	"1inch": {
+		address: "0x111111111117dC0aa78b770fA6A738034120C302",
+		decimals: 18,
+		name: "1Inch Token",
+	},
+	aave: {
+		address: "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9",
+		decimals: 18,
+		name: "AAVE Token",
+	},
+	xrp: {
+		address: "0x628F76eAB0C1298F7a24d337bBbF1ef8A1Ea6A24",
+		decimals: 0,
+		name: "Ripple",
+	},
+	uni: {
+		address: "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
+		decimals: 18,
+		name: "UniSwap Token",
 	},
 };
 
-export const tokens = ["usdt", "bnb", "dai", "usdc"];
+export const tokens = [
+	"usdt",
+	"shib",
+	"dai",
+	"weth",
+	"wbtc",
+	"1inch",
+	"aave",
+	"xrp",
+	"uni",
+];
