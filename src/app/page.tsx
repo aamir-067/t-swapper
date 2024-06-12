bb8bd926d104f9f0bae93c5ea3c26b7d96833620
aamir <aamirkhan@engineer.com>
2024-06-12 12:47:36 +0500

    fix : integrated the quoter function with inputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,32 @@ import { getQuote } from "../utils";
 
 export const userContext = createContext(null);
 export default function Home() {
-  const [inputToken, setInputToken] = useState({ sign: "usdt", amount: 10 });
-  const [outputToken, setOutputToken] = useState({ sign: "shib", amount: 0 });
+  const [inputToken, setInputToken] = useState({ sign: "", amount: 0 });
+  const [outputToken, setOutputToken] = useState({ sign: "", amount: 0 });
+  const [refresh, setRefresh] = useState(false);
+
+
+
+
+  const getQuoteAmount = async () => {
+    if (refresh) {
+      const res = await getQuote(inputToken, outputToken);
+      setOutputToken(prev => { return { ...prev, amount: res } });
+      setRefresh(false);
+    }
+  }
+
+
+
+  if (refresh) {
+    if (inputToken.amount && outputToken.sign !== "" && inputToken.sign !== "") {
+      getQuoteAmount();
+    }
+  }
 
 
   return (
-    <userContext.Provider value={{ inputToken, setInputToken, outputToken, setOutputToken }}>
+    <userContext.Provider value={{ inputToken, setRefresh, setInputToken, outputToken, setOutputToken }}>
       <div className="min-h-screen flex items-center flex-col">
         <button onClick={() => getQuote(inputToken, outputToken)}>click me</button>
         <NavBar />
--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -1,19 +1,45 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { tokens } from '../tokens'
+import { userContext } from '../app/page'
 
 const Inputs = () => {
+    const { setInputToken, setRefresh, setOutputToken, inputToken, outputToken } = useContext(userContext);
+
+
+    console.log(inputToken, outputToken);
+
+
+    const handleChange = (e: React.FormEvent<HTMLSelectElement | HTMLInputElement>, whichOne: string, isInput: boolean) => {
+
+        if (whichOne === "input") {
+
+            if (isInput) {
+                setInputToken({ ...inputToken, amount: Number(e.currentTarget.value) });
+            } else {
+                setInputToken({ ...inputToken, sign: e.currentTarget.value });
+            }
+            console.log("Input Changed : ", e.currentTarget.value);
+        } else {
+            setOutputToken({ ...outputToken, sign: e.currentTarget.value })
+            console.log("Output Changed : ", e.currentTarget.value);
+        }
+        setRefresh(true);
+    }
+
 
     return (
         <div className='border-2 border-black rounded-md w-4/12 flex justify-center py-4 my-4'>
             <div className='flex flex-col w-9/12 gap-2 '>
                 <div className='flex justify-between border-2 rounded-sm border-black/30'>
                     <input
+                        onChange={(e) => handleChange(e, "input", true)}
                         className="flex h-10 w-10/12 rounded-md border bg-transparent px-3 py-2 border-none text-sm placeholder:text-gray-600 outline-none disabled:cursor-not-allowed disabled:opacity-50"
                         type="number"
+                        value={inputToken.amount}
                         placeholder="Token0"
                     />
 
-                    <select defaultValue={""}>
+                    <select onChange={(e) => handleChange(e, "input", false)} defaultValue={""}>
                         <option value={""}>Select Token</option>
                         {
                             tokens.map((token: string, index: number) => {
@@ -28,15 +54,14 @@ const Inputs = () => {
                         className="flex h-10 w-10/12 rounded-md border bg-transparent px-3 py-2 border-none text-sm placeholder:text-gray-600 outline-none disabled:cursor-not-allowed disabled:opacity-50"
                         type="number"
                         readOnly
-                        value={9}
-                        placeholder="Token0"
+                        value={outputToken?.amount}
                     />
 
-                    <select defaultValue={""}>
-                        <option value={""}>Select Token</option>
+                    <select onChange={(e) => handleChange(e, "output", false)} defaultValue={""}>
+                        <option onChange={(e) => setOutputToken({ ...outputToken })} value={""}>Select Token</option>
                         {
                             tokens.map((token: string, index: number) => {
-                                return <option key={index} value={token}>{token}</option>
+                                return token != inputToken?.sign && <option key={index} value={token}>{token}</option>
                             })
                         }
                     </select>
